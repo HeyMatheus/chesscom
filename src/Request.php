32b60c4c3d24a3a080ba606b3e8d9c2cd1c1fc15
HeyMatheus <miliandroxd@gmail.com>
2021-04-28 19:45:49 -0300

    Fix access modifier

--- src/Request.php
+++ src/Request.php
@@ -8,14 +8,14 @@ abstract class Request
 {
     private Client $client;
 
-    public function __construct()
+    function __construct()
     {
         $this->client = new Client([
             'base_uri' => 'https://api.chess.com/pub/',
         ]);
     }
 
-    public function getResponseAsJson($value)
+    protected function getResponseAsJson($value)
     {
         $response = $this->client->get($value)->getBody();
 
